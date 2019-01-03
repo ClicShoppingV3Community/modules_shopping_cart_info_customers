0bc96b18814dfe8ff531d7521d6ad3e4d4e35bfe
ClicShopping - e-commerce Solution B2B B2C <39338579+ClicShopping@users.noreply.github.com>
2019-01-03 20:25:22 +0100

    new module

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# ms_shopping_cart_info_customers
+# modules_shopping_cart_info_customers
+
 Allow to display a specific infomation (security, privacy, contact...) inside the shopping cart
+
+This module contains
+
+- The language files in English and French
+- The css file in French and English
+- The module
+  
+Via the installation system administration ClicShopping
+
+Technical Prerequisites: None
+
+License : GPL 2 - MIT
+
+Modules:
+
+- Compatibility: >= version 3.0
+- Multi languages
+
+Recommendation and documentation specific use :
+The module is installed in the Default template.
+If you have another template, you must copy the files inside your new template.
+
+
+Implementation:
+
+- Install the module 
+- Activate the module Design / Order Process / Shopping cart
+
+- Copy the modules_shopping_cart_info_customers.json into ClicShopping/Work/Cache/Github (manual installation)
+
+![image](https://github.com/ClicShoppingV3Community/modules_shopping_cart_info_customers/blob/master/ModuleInfosJson/image.png)
+
+
---
+++ sources/template/Default/modules/modules_shopping_cart/content/shopping_cart_info_customers.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ *
+ *  @copyright 2008 - https://www.clicshopping.org
+ *  @Brand : ClicShopping(Tm) at Inpi all right Reserved
+ *  @Licence GPL 2 & MIT
+ *  @licence MIT - Portion of osCommerce 2.4
+ *  @Info : https://www.clicshopping.org/forum/trademark/
+ *
+ */
+
+  use ClicShopping\OM\HTML;
+  use ClicShopping\OM\CLICSHOPPING;
+?>
+<div class="col-md-<?php echo $content_width; ?> <?php echo $position; ?>">
+  <div class="separator"></div>
+  <div class="infoCustomersHeading">
+    <table width="100%"  border="0" cellspacing="0" cellpadding="5" class="shoppingCartInfoCustomersTable">
+      <tr>
+        <th width="20%" align="center"><strong><?php echo CLICSHOPPING::getDef('module_shopping_cart_info_customers_heading_payment'); ?></th>
+        <th width="20%" align="center"><strong><?php echo CLICSHOPPING::getDef('module_shopping_cart_info_customers_heading_shipping'); ?></strong></th>
+        <th width="20%" align="center"><strong><?php echo CLICSHOPPING::getDef('module_shopping_cart_info_customers_heading_private'); ?></strong></th>
+        <th width="20%" align="center"><strong><?php echo CLICSHOPPING::getDef('module_shopping_cart_info_customers_heading_contact_us'); ?></strong></th>
+      </tr>
+      <tr valign="top">
+        <td><?php echo CLICSHOPPING::getDef('module_shopping_cart_info_customers_text_payment'); ?><p class="text-md-center;"><?php echo  HTML::image($CLICSHOPPING_Template->getDirectoryTemplateImages() . 'logos/payment/3_cb.png'); ?></p></td>
+        <td><?php echo CLICSHOPPING::getDef('module_shopping_cart_info_customers_text_shipping') . ' ' . $free_amount; ?></td>
+        <td><?php echo CLICSHOPPING::getDef('module_shopping_cart_info_customers_text_private'); ?></td>
+        <td><?php echo CLICSHOPPING::getDef('module_shopping_cart_info_customers_text_contact_us'); ?><?php echo HTML::link(CLICSHOPPING::link(null,'Info&Contact'), STORE_OWNER_EMAIL_ADDRESS); ?></td>
+      </tr>
+    </table>
+  </div>
+</div>
